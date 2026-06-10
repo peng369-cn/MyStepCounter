dd6f53888ef3124a130283f7d0afcdd21795bb68
Peng <1874790971@qq.com>
2026-06-10 23:11:15 +0800

    compileSdk/targetSdk降回35，CI用稳定版API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 android {
     namespace = "com.pengchangwei.stepcounter"
 
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.pengchangwei.stepcounter"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
